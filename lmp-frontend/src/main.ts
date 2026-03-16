c40214ef7962ec011c9d6e718ba0984e5cf1ad4e
BassCode <patricklemmy.nangandzengue@student.efpl.be>
2026-03-16 14:36:01 +0100

    fix: move global polyfill to index.html (ES module imports are hoisted)

--- lmp-frontend/src/main.ts
+++ lmp-frontend/src/main.ts
@@ -1,9 +1,3 @@
-/**
- * Polyfill: sockjs-client expects `global` to be defined (Node.js env).
- * In browser environments we alias it to `window`.
- */
-(window as any).global = window;
-
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
