78995b695208baf9328c0db3a216bfc870fbb6f9
BassCode <patricklemmy.nangandzengue@student.efpl.be>
2026-04-27 21:30:22 +0200

    fix: ajout import manquant ConfigurableApplicationContext dans SentryBootstrapListener

--- src/main/java/com/lmp/shared/config/SentryBootstrapListener.java
+++ src/main/java/com/lmp/shared/config/SentryBootstrapListener.java
@@ -6,6 +6,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.ConfigurableBootstrapContext;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.SpringApplicationRunListener;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.core.env.ConfigurableEnvironment;
 
 /**
